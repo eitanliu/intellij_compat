043cf704be72dbbafb737d68ef546fe93a155b0e
Eitan Liu <eitanliu@gmail.com>
2023-12-06 11:05:24 +0800

    compatible with after 223.7126 versions

--- src/main/kotlin/com/eitanliu/intellij/compat/extensions/ApplicationExt.kt
+++ src/main/kotlin/com/eitanliu/intellij/compat/extensions/ApplicationExt.kt
@@ -2,13 +2,22 @@ package com.eitanliu.intellij.compat.extensions
 
 import com.eitanliu.intellij.compat.application.EDTCompat
 import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.EDT
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import java.util.*
 import kotlin.concurrent.schedule
+import kotlin.coroutines.CoroutineContext
+
+private val edtContext: CoroutineContext = try {
+    // com.intellij.openapi.application.CoroutinesKt.getEDT(Dispatchers)
+    Dispatchers.EDT
+} catch (e: Throwable) {
+    Dispatchers.EDTCompat
+}
 
 // val ApplicationScope = MainScope()
-val ApplicationScope = CoroutineScope(Dispatchers.EDTCompat)
+val ApplicationScope = CoroutineScope(edtContext)
 
 fun Application.invokeLater(delay: Long, runnable: Runnable) {
     Timer().schedule(delay) { invokeLater(runnable) }
